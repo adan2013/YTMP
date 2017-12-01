0a73842f2891a01acc4dc2e168b356d776d7d79a
adan2013 <adan2013@poczta.fm>
2017-12-01 16:13:11 +0100

    version checker test 2

--- YTMP/updateform.Designer.vb
+++ YTMP/updateform.Designer.vb
@@ -24,6 +24,9 @@ Partial Class updateform
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(updateform))
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnlink = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -36,11 +39,46 @@ Partial Class updateform
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Dostępna jest aktualizacja aplikacji" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "YouTube Media Player!"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(351, 30)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "1.Aby zaktualizować aplikację należy przejść do serwisu GitHub" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "dostępnego pod li" &
+    "nkiem podanym poniżej:"
+        '
+        'btnlink
+        '
+        Me.btnlink.AutoSize = True
+        Me.btnlink.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.btnlink.Location = New System.Drawing.Point(30, 114)
+        Me.btnlink.Name = "btnlink"
+        Me.btnlink.Size = New System.Drawing.Size(232, 15)
+        Me.btnlink.TabIndex = 2
+        Me.btnlink.TabStop = True
+        Me.btnlink.Text = "Repozytorium aplikacji w serwisie GitHub"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(378, 75)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = resources.GetString("Label3.Text")
+        '
         'updateform
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(484, 262)
+        Me.ClientSize = New System.Drawing.Size(424, 232)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnlink)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -56,4 +94,7 @@ Partial Class updateform
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnlink As LinkLabel
+    Friend WithEvents Label3 As Label
 End Class
--- YTMP/updateform.vb
+++ YTMP/updateform.vb
@@ -1,3 +1,10 @@
 ﻿Public Class updateform
 
+    Private Sub btnlink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles btnlink.LinkClicked
+        Try
+            Process.Start("https://github.com/adan2013/YTMP/releases")
+        Catch ex As Exception
+            MsgBox("Wystąpił błąd podczas uruchamiania przeglądarki internetowej!", MsgBoxStyle.Critical, "YTMP")
+        End Try
+    End Sub
 End Class
