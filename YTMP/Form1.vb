174f79682206014ef7bda9cd2f57d6d5fa91a5ad
adan2013 <adan2013@poczta.fm>
2018-05-17 11:26:20 +0200

    FINAL 7.1

--- YTMP/Form1.vb
+++ YTMP/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Imports Microsoft.WindowsAPICodePack.Taskbar
 Public Class Form1
 
-    Public wersja As String = "v7.0"
+    Public wersja As String = "v7.1"
     Dim WithEvents kb As KeyboardHook = New KeyboardHook()
     Public rewindstate As Double = -1
 
